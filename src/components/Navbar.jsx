ddec0178e1a4e76d02ebe6819bb33f7038fc573e
Sandeep Kumar Hari <sandeepkumarhari@Sandeeps-MacBook-Air.local>
2024-04-12 02:41:19 +0530

    version 1.2 changed the css of home button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,9 +6,9 @@ const Navbar = () => {
     <header className="header">
       <NavLink
         to="/"
-        className="w-40 h-30 rounded-lg bg-white items-center justify-center flex font-bold "
+        className="w-10 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md"
       >
-        <p className="blue-gradient_text">SANDEEPs WORLD</p>
+        <p className="blue-gradient_text">SKH</p>
       </NavLink>
       <nav className="flex text-lg gap-7 font-medium">
         <NavLink
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -54,8 +54,9 @@ const About = () => {
         <h3 className="subhead-text">Work Experience.</h3>
         <div className="mt-5 flex flex-col gap-3 text-slate-500">
           <p>
-            I've worked with all sorts of companies, leveling up my skills and
-            teaming up with smart people. Here's the rundown:
+            I have worked with companies spanning diverse technological
+            landscapes, continually refining my skills while collaborating with
+            talented individuals. Here's an overview:"
           </p>
         </div>
 
